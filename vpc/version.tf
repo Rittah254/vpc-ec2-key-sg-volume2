cf90dbfa6bc2735eac1fb487b06f552f9d573d4b
rittah <rittahjustine@yahoo.com>
2025-03-30 18:18:58 -0500

    feat(utc-app) added code for dev env build

---
+++ vpc/version.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.79.0"
+    }
+  }
+}
+
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "rw-w7-terraformstatebucket"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+    use_lockfile = true
+  }
+}
